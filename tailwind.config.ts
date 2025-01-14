a2a608945be02612f9e7bd1d8c3900624672b9b9
Dratatin <matchirouze@gmail.com>
2025-01-14 11:58:39 +0100

    Add graphic charter

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,17 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {},
+		colors: {
+			'primary': '#120F26',
+			'secondary': '#021F59',
+			'tertiary': '#061940',
+			'light': '#F2F2F2',
+			'dark': '#0D0D0D',
+		},
+		fontFamily: {
+			sans: ['Gabarito', 'sans-serif']
+		},
 	},
 
 	plugins: [typography, forms]
